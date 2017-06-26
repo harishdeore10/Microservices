9f769d85ccc49bcdfcd0d73a4579a2f45efcc8ee
harishdeore10 <harishdeore10@gmail.com>
2017-06-26 19:36:18 +0530

    First commit

--- src/main/java/com/hello/Hello.java
+++ src/main/java/com/hello/Hello.java
@@ -2,12 +2,14 @@ package com.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class Hello {
 
 	@RequestMapping(value="/")
