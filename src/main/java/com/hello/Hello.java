bc547d6b462cfa0eca990d1934fbe4c99afdc8d4
harishdeore10 <harishdeore10@gmail.com>
2017-06-26 18:57:05 +0530

    First commit

---
+++ src/main/java/com/hello/Hello.java
@@ -0,0 +1,23 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Hello {
+
+	@RequestMapping(value="/")
+	@ResponseBody
+	public String sayHello(){
+		return "Welcome to Spring Boot with Goocle Cloud Platform .";
+	}
+	
+	public static void main(String [] args){
+		SpringApplication.run(Hello.class, args);
+	}
+	
+}
