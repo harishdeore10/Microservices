b86ce41216f0c75246073633a95329b1f869ef26
harishdeore10 <harishdeore10@gmail.com>
2017-06-28 21:48:48 +0530

    First commit

--- src/main/java/com/hello/Hello.java
+++ src/main/java/com/hello/Hello.java
@@ -1,25 +1,44 @@
 package com.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
-@EnableDiscoveryClient
 public class Hello {
 
-	@RequestMapping(value="/")
-	@ResponseBody
-	public String sayHello(){
-		return "Welcome to Spring Boot with Goocle Cloud Platform . having git hub";
+	@Autowired
+	public RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
-	
-	public static void main(String [] args){
+
+	@RequestMapping(value = "/input", method = RequestMethod.POST)
+	@ResponseBody
+	public String input(@RequestBody String input) {
+		String fileData = restTemplate.getForObject("https://storage.googleapis.com/user-test-data-1/Hello.txt",
+				String.class);
+
+		if (fileData.equals(input)) {
+			return "True";
+		} else {
+			return "False";
+		}
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(Hello.class, args);
 	}
-	
+
 }
